7de455d46a356ca72807c7930a24e8cbe057dbbd
babofitos <scvfighting@gmail.com>
2014-12-13 04:10:13 -0500

    initial commit

---
+++ scrape.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+  "fmt"
+  "github.com/PuerkitoBio/goquery"
+)
+
+var url = "http://www3.nhk.or.jp/news/easy/k10013455581000/k10013455581000.html"
+
+func Scrape() {
+  doc, err := goquery.NewDocument(url)
+  if err != nil {
+    panic(err)
+  }
+
+  title := doc.Find("title").First().Text()
+  fmt.Printf("%v\n", title)
+
+  newsarticle := doc.Find("#newsarticle")
+
+
+  //TODO:filter out even newlines between p's
+  var text string
+  newsarticle.Children().Each(func(i int, s *goquery.Selection) {
+    paragraph := s
+    text += getTextNodes(paragraph)
+  })
+  fmt.Printf("%v", text)
+}
+
+func getTextNodes(s *goquery.Selection) string {
+  var output string
+
+  nodes := s.Contents()
+
+  if nodes.Length() < 1 {
+    //base case, text node
+    return s.Text()
+  }
+  nodes.Each(func(i int, node *goquery.Selection) {
+    if !node.Is("rt") {
+      output += getTextNodes(node)
+    }
+  })
+  return output
+}
+
+func main() {
+  Scrape()
+}
