fa41afe5a41a2dc32865035fea1ebba068599457
babofitos <scvfighting@gmail.com>
2014-12-14 21:40:49 -0500

    make it a package

--- scrape.go
+++ scrape.go
@@ -1,38 +1,47 @@
-package main
+package nhkeasy
 
 import (
-	"fmt"
+	"errors"
 	"github.com/PuerkitoBio/goquery"
+	"net/http"
 	"strings"
 )
 
-func Scrape(url, separator string) {
-	doc, err := goquery.NewDocument(url)
+func Scrape(url, separator string) (string, string, error) {
+	resp, err := request(url)
 	if err != nil {
-		panic(err)
+		return "", "", err
 	}
-
+	doc, err := getDocument(resp)
+	if err != nil {
+		return "", "", err
+	}
 	title := doc.Find("title").Text()
+	title = prettyTitle(title)
 	var text string
-
-	fmt.Println(prettyTitle(title))
-
 	newsarticle := doc.Find("#newsarticle")
 	newsarticle.Children().Each(func(i int, paragraph *goquery.Selection) {
-		if paragraph.Children().Length() > 0 {
-			text += getTextNodes(paragraph)
+		text += getTextNodes(paragraph)
+		if paragraph.Next().Children().Length() > 0 {
 			text += separator
 		}
 	})
+	return title, text, err
+}
 
-	fmt.Print(text)
+func getDocument(resp *http.Response) (*goquery.Document, error) {
+	doc, err := goquery.NewDocumentFromResponse(resp)
+	defer resp.Body.Close()
+	if err != nil {
+		err = errors.New("Could not parse response body")
+		return nil, err
+	}
+	return doc, nil
 }
 
 func getTextNodes(s *goquery.Selection) string {
 	var output string
-
 	nodes := s.Contents()
-
 	if nodes.Length() < 1 {
 		//base case, text node
 		return s.Text()
@@ -49,14 +58,15 @@ func prettyTitle(title string) string {
 	return strings.Split(title, "|")[1]
 }
 
-func main() {
-	var (
-		url       string
-		separator string
-	)
-	fmt.Print("Input URL:")
-	fmt.Scan(&url)
-	fmt.Print("Input Separator:")
-	fmt.Scan(&separator)
-	Scrape(url, separator)
+func request(url string) (*http.Response, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		err = errors.New("HTTP Protocol Error")
+		return nil, err
+	}
+	if resp.StatusCode == 404 {
+		err = errors.New("Status code 404. Check URL")
+		return nil, err
+	}
+	return resp, nil
 }
